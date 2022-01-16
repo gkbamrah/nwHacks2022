56007b028be8ad816bdd45edfb250418c78dc794
nishant-sr <nishant.srinivasan236@gmail.com>
2022-01-15 23:00:13 -0800

    done backend, starting frontend

--- extension/public/script.js
+++ extension/public/script.js
@@ -1,13 +1,84 @@
-console.log("hEY");
+let token = 
 
-url = 'https://canvas.ubc.ca/api/v1/courses/86703/assignments'
+function getCourses() {
+    let url = 'https://canvas.ubc.ca/api/v1/courses'
+    let courses = [];
+    return new Promise((resolve) => {
+        fetch(url).then(response => response.json()).then((data) => {
+            for (let course of data) {
+                if (course.name) {
+                    courses.push([course.id, course.name]);
+                }
+            }
+            return resolve(courses);
+        });
+    });
+}
 
-fetch(url).then(response => response.json()).then(data => console.log(data))
+function getAllAssignments(courses) {
+    let promises = [];
 
-// application , wrapper,main,not_right_side,
-// content-wrapper , content ,assignmentGroups
-// collectionViewItems ig-list , 
-// item-group-condensed
-// assignment_group_past_assignments
-// collectionViewItems ig-list draggable
-// assignment sort-disabled search_show
+    for (let course of courses) {
+
+        promises.push(getAssignments(course[0]));
+    }
+
+    return Promise.all(promises).then(allAssignments => {
+
+        return allAssignments;
+    })
+}
+
+function getAssignments(courseId) {
+    let assignments = [];
+    let url = 'https://canvas.ubc.ca/api/v1/courses/' + courseId + '/assignments';
+    return new Promise((resolve) => {
+        fetch(url).then(response => response.json()).then((data) => {
+            for (let assignment of data) {
+                if (assignment.due_at) {
+                    assignments.push([assignment.name, assignment.due_at,courseId])
+                }
+            }
+
+            return resolve(assignments);
+        });
+    });
+}
+
+function start() {
+    return getCourses().then((courses) => {
+        return getAllAssignments(courses).then((allAssignments) => {
+            return addTasks(allAssignments);
+        });
+    });
+}
+
+start();
+
+
+
+function addTasks(tasks){
+    for(let task of tasks){
+        if(task.length>0){
+            for(let ass of task){
+                addTask(ass[0],ass[1],ass[2])
+                console.log(ass[1])
+            }
+        }
+    }
+}
+
+function addTask(name,timedate,id){
+    console.log(name)
+    console.log(timedate)
+    addSection(id)
+    fetch('https://api.todoist.com/rest/v1/tasks',{
+    method: 'POST',
+    body : JSON.stringify({"content":name,"due_datetime":timedate,"section_id":id}),
+    headers:{
+        "Content-Type": "application/json",
+        "X-Request-Id": "$(uuidgen)",
+        "Authorization": "Bearer " + token
+    }
+    }).then(response => response.json()).then((data) => console.log(data))
+}
